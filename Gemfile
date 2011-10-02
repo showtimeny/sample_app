b7afc4ba85ccfbb3120b26409e70fe07dc44f4ec
Jackson Coakley <jackson.coakley@gmail.com>
2011-10-02 14:34:25 -0400

    added password and encrypted_password attributes

--- Gemfile
+++ Gemfile
@@ -9,11 +9,11 @@ gem 'sqlite3-ruby', '1.2.5', :require => 'sqlite3'
 
 
 group :development, do
-   gem 'rspec-rails'
+   gem 'rspec-rails', '2.0.0.beta.18'
 
 end
 
 group :test do
-	gem 'rspec'
+	gem 'rspec', '2.0.0.beta.18'
 	gem 'spork'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,30 @@
 class User < ActiveRecord::Base
   
-  attr_accessible :name, :email
+  attr_accessible :name, :email, :password, :password_confirmation
+  attr_accessor :password
   
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
   validates :name, :presence    => true,
-                    :length     => {:maximum => 50}
+                    :length     => { :maximum => 50 }
   validates :email, :presence   => true,
-                    :format     => {:with => email_regex },
-                    :uniqueness => {:case_sensitive => false }
+                    :format     => { :with => email_regex },
+                    :uniqueness => { :case_sensitive => false }
   
+  validates :password, :presence => true,
+                        :confirmation => true,
+                        :length => { :within => 6..40 }
+                      
+  before_save :encrypt_password
   
+  private
+  def encrypt_password
+    self.encrypted_password = encrypt(self.password)
+  end
   
-  
-  
+  def encrypt(string)
+    string
+  end
   
   
   
